b6ced3560ae1a687cf131880a1e589d7aa08f690
Harshvi02 <230493131002.harshvi@gmail.com>
2026-02-23 15:48:21 +0530

    improve global styles with base reset and typography

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,16 +1,38 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  * {
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%; /* 1rem = 10px */
   }
 
   body {
-    font-family: sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+
     background-color: #f9fafb;
+    color: #374151;
+    line-height: 1.6;
+    font-size: 1.6rem;
   }
+
+  h1 {
+    font-size: 2.4rem;
+    margin-bottom: 2rem;
+  }
+
+  button {
+    font-family: inherit;
+  }
+
 `;
 
 export default GlobalStyles;
