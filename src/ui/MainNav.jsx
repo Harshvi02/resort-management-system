3da003df5bcf275410c7de4def80f01f0b4b64c5
Harshvi02 <230493131002.harshvi@gmail.com>
2026-02-23 12:26:34 +0530

    add navigation links with active styling in sidebar

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,23 +1,48 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 const NavList = styled.ul`
   list-style: none;
   margin-top: 30px;
 `;
 
-const NavItem = styled.li`
-  margin-bottom: 15px;
+const StyledNavLink = styled(NavLink)`
+  display: block;
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  text-decoration: none;
+  color: #374151;
+  border-radius: 6px;
+
+  &.active {
+    background-color: #2563eb;
+    color: white;
+  }
+
+  &:hover {
+    background-color: #e5e7eb;
+  }
 `;
 
 function MainNav() {
   return (
     <nav>
       <NavList>
-        <NavItem>Dashboard</NavItem>
-        <NavItem>Cabins</NavItem>
-        <NavItem>Bookings</NavItem>
-        <NavItem>Guests</NavItem>
-        <NavItem>Settings</NavItem>
+        <li>
+          <StyledNavLink to="/dashboard">Dashboard</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/cabins">Cabins</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">Bookings</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/guests">Guests</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/settings">Settings</StyledNavLink>
+        </li>
       </NavList>
     </nav>
   );
